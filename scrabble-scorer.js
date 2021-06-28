9fa1e3b36daa3fec52147892b359871d15cbeac1
Victoria Rouser <vic86rous@gmail.com>
2021-06-28 00:37:47 +0000

    added initial prompt function

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -32,11 +32,14 @@ function oldScrabbleScorer(word) {
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
-function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
-};
+function initialPrompt(prompts) {
+  let intro = input.question("Let's play some Scrabble! \n\n\n Enter a word: ")
+  return intro[prompts];
+}
 
-let simpleScore;
+
+
+let simpleScore = "";
 
 let vowelBonusScore;
 
